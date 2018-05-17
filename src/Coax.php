af2c59684b664d2a249fa4c8eacacbbfb27d6eb6
Jonathon Ball <jonathon.ball@gmail.com>
2018-05-16 21:25:45 -0400

    necroed some functions, yay revision controlgit status

--- src/Coax.php
+++ src/Coax.php
@@ -2,6 +2,35 @@
 
 namespace Coax;
 
+use Coax\CoaxOptions;
+
 class Coax {
+
+    public function __construct($arguments = []) {
+        $this->setArguments($arguments);
+    }
+
+    /**
+     * Sets the values to use as argv[]
+     * @param array $arguments
+     * @return $this
+     */
+    public function setArguments($arguments = []) {
+        if (! is_array($arguments)) throw new Exception('Coax expects array');
+        if (count($arguments)) {
+            $this->_argv = array_merge([basename(__FILE__)], $arguments);
+        } else {
+            $this->_argv = $GLOBALS['argv'];
+        }
+        return $this;
+    }
+
+    /**
+     * Gets the values representing argv[]
+     * @return array
+     */
+    public function getArguments() {
+        return $this->_argv;
+    }
 
 }
