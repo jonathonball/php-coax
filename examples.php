f561f815173b79c587bd2b740e028fa9423fca00
Jonathon Ball <jonathon.ball@gmail.com>
2018-05-18 23:21:43 -0400

    added semi-working examples

--- examples.php
+++ examples.php
@@ -2,15 +2,20 @@
 
 require_once(__DIR__ . '/vendor/autoload.php');
 
-/*
+use Coax\Coax;
 use Coax\CoaxOption;
+
+/**
+ * This is a single option with every possible method applied.
+ * It will not parse. This is just a silly example.
+ */
 $test = new CoaxOption('--example');
 $test->alias(['b', 'c'])
      ->castToArray()
      ->castToBoolean()
      ->choices(['test', 'test2'])
      ->coerce(function($value) {
-         return $value > 1;
+         return ($value > 1); // force to boolean
      })->conflicts('x', 'y', 'z')
      ->count()
      ->defaultTo('something')
@@ -23,19 +28,41 @@ $test->alias(['b', 'c'])
      ->nargs(10)
      ->numberLike()
      ->stringLike();
-var_dump($test);
-*/
+//var_dump($test);
 
-use Coax\Coax;
-use Coax\CoaxOption;
-
+/**
+ * This is the sames as..
+ */
 $coax = new Coax();
-$coax->options()->alias('a', ['b', 'c']);
+$coax->options()
+     ->alias('a', ['b', 'c'])
+     ->nargs('a', 5)
+     ->demand('x')
+     ->count('x');
+//var_dump($coax);
 
+/**
+ * this, is the same as..
+ */
 $coax = new Coax();
-$coax->options('a')->alias(['b', 'c']);
+$coax->option('a')
+     ->alias(['b', 'c'])
+     ->nargs(5);
+$coax->option('x')
+     ->demand()
+     ->count();
+//var_dump($coax);
 
-$coax = new Coax();
+/**
+ * This
+ */
 $a = new CoaxOption('a');
-$a->alias(['b', 'c']);
-$coax->options($a);
+$a->alias(['b', 'c'])
+  ->nargs(5);
+$x = new CoaxOption('x');
+$x->demand()
+  ->count();
+$coax = new Coax();
+$coax->option($a);
+$coax->option($x);
+//var_dump($coax);
