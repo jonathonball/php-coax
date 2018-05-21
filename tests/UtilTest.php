c26e9c2a1b9617db8cc70b2d1ad677286e470ba6
Jonathon Ball <jonathon.ball@gmail.com>
2018-05-20 22:40:37 -0400

    started added tests

---
+++ tests/UtilTest.php
@@ -0,0 +1,20 @@
+<?php 
+
+use PHPUnit\Framework\TestCase;
+
+use Coax\Util;
+use Coax\CoaxOption;
+
+class EmailTest extends TestCase {
+
+    protected $exampleOption;
+
+    protected function setUp() {
+        $this->exampleOption = new CoaxOption('e');
+    }
+
+    public function testIsCoaxOption() {
+        $this->assertTrue(Util::is_coax_option($this->exampleOption));
+    }
+
+}
