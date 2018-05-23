e995366869a5bb8e261a4ca4e6276c199b346e7e
Jonathon Ball <jonathon.ball@gmail.com>
2018-05-22 21:58:32 -0400

    got rid of foreach loops and changed internal data members into data providers

--- tests/CoaxOptionTest.php
+++ tests/CoaxOptionTest.php
@@ -5,30 +5,38 @@ use Coax\CoaxOption;
 
 class CoaxOptionTest extends TestCase {
 
-    protected $goodTags = ['e', 'example'];
-    protected $badTags = [1];
-
-    protected function setUp() {
-        array_push($this->badTags, new stdClass);
-    }
-    
-    public function testCanCreateFromString() {
-        foreach ($this->goodTags as $tag) {
-            $this->assertInstanceOf(
-                'Coax\CoaxOption',
-                new CoaxOption($tag)
-            );
-        }
+    public function goodTagProvider() {
+        return [
+            ['e'],
+            ['example']
+        ];
     }
 
-    public function testCanCreateFromCoaxOption() {
-        foreach ($this->goodTags as $tag) {
-            $coaxOption = new CoaxOption($tag);
-            $this->assertInstanceOf(
-                'Coax\CoaxOption',
-                $coaxOption
-            );
-        }
+    public function badTagProvider() {
+        return [
+            [1],
+            [new stdClass]
+        ];
+    }
+
+    /**
+     * @dataProvider goodTagProvider
+     */
+    public function testCanCreateFromString($tag) {
+        $this->assertInstanceOf(
+            'Coax\CoaxOption',
+            new CoaxOption($tag)
+        );
+    }
+
+    /**
+     * @dataProvider goodTagProvider
+     */
+    public function testCanCreateFromCoaxOption($tag) {
+        $this->assertInstanceOf(
+            'Coax\CoaxOption',
+            new CoaxOption($tag)
+        );
     }
 
 }
